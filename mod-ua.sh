3318172da217c3f1f91b4380dcb45e4dfe88add1
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-05-27 10:33:14 +0200

    update repos before modifying

--- mod-ua.sh
+++ mod-ua.sh
@@ -2,11 +2,28 @@
 
 client=$1
 netinst="../raspbian-ua-netinst"
+branch=$(cat ../netinst.branch)
 
 if [ "$client" = "" ]; then
   echo "Usage: make.sh <hostname>"
   exit 1
 fi
+
+echo "Update the mod-ua files..."
+git pull
+git fetch origin
+git checkout $branch
+git reset --hard origin/$branch && \
+git clean -f -d
+
+echo "Update the raspbian-ua-netinst files..."
+pushd ../raspbian-ua-netinst
+git pull
+git fetch origin
+git checkout $branch
+git reset --hard origin/$branch && \
+git clean -f -d
+popd
 
 echo "Put modifications in place"
 cp installer-config.txt $netinst/
