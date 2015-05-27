f03f4783ad25a763f6d10d754fbeba003ff469b8
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-05-27 10:43:11 +0200

    test exit code of pushd

--- mod-ua.sh
+++ mod-ua.sh
@@ -5,7 +5,7 @@ netinst="../raspbian-ua-netinst"
 branch=$(cat ../netinst.branch)
 
 if [ "$client" = "" ]; then
-  echo "Usage: make.sh <hostname>"
+  echo "Usage: mod-ua.sh <hostname>"
   exit 1
 fi
 
@@ -17,7 +17,9 @@ git reset --hard origin/$branch && \
 git clean -f -d
 
 echo "Update the raspbian-ua-netinst files..."
-pushd ../raspbian-ua-netinst
+pushd ../raspbian-ua-netinst/
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
 git pull
 git fetch origin
 git checkout $branch
@@ -31,7 +33,8 @@ cp post-install.txt $netinst/
 cp -r config $netinst/
 
 echo "Building image"
-pushd ../raspbian-ua-netinst
+pushd ../raspbian-ua-netinst/
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 ./clean.sh
 ./update.sh
