f81c393c7228675c699306c57556b4754fe78022
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-05-27 09:35:49 +0200

    initial changes

--- mod-ua.sh
+++ mod-ua.sh
@@ -1,15 +1,27 @@
 #! /bin/sh
 
 client=$1
+netinst="../raspbian-ua-netinst"
 
 if [ "$client" = "" ]; then
   echo "Usage: make.sh <hostname>"
   exit 1
 fi
 
+echo "Put modifications in place"
+cp installer-config.txt $netinst/
+cp post-install.txt $netinst/
+cp -r config $netinst/
+
+echo "Building image"
+pushd ../raspbian-ua-netinst
+
 ./clean.sh
 ./update.sh
 ./build.sh
-#./buildroot
 
 sed -i "s/raspberrypi/${client}/" ./bootfs/installer-config.txt
+
+#./buildroot
+
+popd
