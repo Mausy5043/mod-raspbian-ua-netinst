e2c7899b7cdd150baa93ac2a8bfcd6e516fe82aa
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-05-27 11:05:05 +0200

    20150527.1105

--- mod-ua.sh
+++ mod-ua.sh
@@ -8,7 +8,7 @@ if [ ! -d $netinst ]; then
   echo "A clone of raspbian-ua-netinst could not be found."
   exit 1
 fi
-if [ ! -d $branch ]; then
+if [ ! -e $branch ]; then
   echo "Could not find "$branch
   echo "This file should contain the name of the branch to be used."
   echo "Both the raspbian-ua-netinst branch and the mod-raspbian-ua-netinst branch must have the same name."
