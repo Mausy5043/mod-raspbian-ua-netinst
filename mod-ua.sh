bb5140ae3f7fd5ec907345dff2cb434007c9f851
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-01-01 18:24:55 +0100

    Double quote to prevent globbing and word splitting.

--- mod-ua.sh
+++ mod-ua.sh
@@ -46,8 +46,8 @@ pushd $netinst/
 
   git pull
   git fetch origin
-  git checkout $branch
-  git reset --hard origin/$branch && \
+  git checkout "$branch"
+  git reset --hard "origin/$branch" && \
   git clean -f -d
 popd
 
