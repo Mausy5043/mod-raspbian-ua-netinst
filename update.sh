a0162d49b6bdde59ead90959e15b109b909dfc59
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2016-06-18 08:53:12 +0200

    Wpa supplicant 2 (#17)
    * 20160424T1847
    
    * use archey-m.py
    
    * separate update script
    
    * chmod *.sh
    
    * lsb-release package added
    
    * move package installation to appropriate repos
    
    * set timeout
    
    * update
    
    * prevent endless retries
    * exit if command fails to prevent overwriting/deleting files
    
    * additional settings

---
+++ update.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+netinst="../raspbian-ua-netinst"
+branch="../netinst.branch"
+wpa="../wpa.conf"
+
+# Check if `netinst.branch` file exists
+# This file contains the name of the branch that should be used.
+if [ ! -e $branch ]; then
+  echo "Could not find "$branch
+  echo "This file should contain the name of the branch to be used."
+  echo "Both the branchname of raspbian-ua-netinst and the branchname of mod-raspbian-ua-netinst must be the same."
+  exit 1
+fi
+branch=$(cat $branch)
+
+echo ""
+echo "*********"
+echo "Updating the mod-ua files..."
+echo "*********"
+git pull
+git fetch origin
+git checkout "$branch"
+git reset --hard "origin/$branch" && \
+git clean -f -d
+chmod +x *.sh
